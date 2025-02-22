8f5b5c88a2ab490a323f3c7445746efbc0aecdbe
piyushgoyall <piyushgoyal3003@gmail.com>
2025-02-22 15:38:53 -0500

    list-accounts

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,17 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    Lists all Accounts
+    This endpoint will list all Accounts in the database
+    """
+    app.logger.info("Request to list all Accounts")
+    accounts = Account.all()
+    results = [account.serialize() for account in accounts]
+    return jsonify(results), status.HTTP_200_OK
+
 
 
 ######################################################################
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,3 +166,12 @@ class TestAccountService(TestCase):
 
         resp = self.client.get(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_list_accounts(self):
+        """It should List all Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
